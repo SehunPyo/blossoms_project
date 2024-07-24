f389ae66e8f12cad1348cf515a83e287993d7c16
SehunPyo <hyu02130@naver.com>
2024-07-24 18:04:49 +0900

    Under Dev. 20240724 ver.01

--- src/components/KakaoLogin/KakaoLogin.js
+++ src/components/KakaoLogin/KakaoLogin.js
@@ -3,24 +3,22 @@ import React from 'react';
 import './KakaoLogin.css';
 
 
-const KakaoLogin = ()=>
-    {
+function KakaoLogin() {
         const Rest_api_key='556805e91e800197a8026aa2dfe3c65b' //REST API KEY 입력
-        const redirect_uri = 'http://localhost:3000/auth' //Redirect URI 입력
+        const redirect_uri = 'http://localhost:4000/auth/callback'; // 서버의 Redirect URI 입력
 
-        // oauth 요청 URL
-        const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
-        const handleLogin = ()=>{
-            window.location.href = kakaoURL
-        }
-        return(
-        <>
-        <button onClick={handleLogin}>KAKAO 로그인</button>
-        </>
-        )
-    }
-    export default KakaoLogin
-
-
-
+        // auth 요청 URL
+        const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
     
+        const handleLogin = () => {
+            window.location.href = kakaoURL;
+        };
+    
+        return (
+            <>
+                <button onClick={handleLogin}>KAKAO 로그인</button>
+            </>
+        );
+    };
+    
+    export default KakaoLogin;
